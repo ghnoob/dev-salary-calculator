a3a0605b27cc3500dd9fd1b987689f89f1a23c4d
ghnoob <ptnchk.rodrigo@gmail.com>
2021-09-21 12:28:53 -0300

    style rate form

--- src/components/RateForm.vue
+++ src/components/RateForm.vue
@@ -1,64 +1,93 @@
 <template>
-  <form @submit.prevent="submit">
-    <ul class="wrapper">
-      <li class="form-row">
-        <label for="technology">Tecnología</label>
-        <select id="technology" v-model.number="newRate.technology_id" required>
-          <option v-if="search" value="all" class="search-all">Todas</option>
-          <option v-for="tech in technologies" :key="tech.id" :value="tech.id">
-            {{ tech.name }}
-          </option>
-        </select>
-      </li>
-      <li class="form-row">
-        <label for="seniority">Seniority</label>
-        <select id="seniority" v-model="newRate.seniority" required>
-          <option v-if="search" value="all" class="search-all">Todas</option>
-          <option value="junior">Junior</option>
-          <option value="semi_senior">Semi senior</option>
-          <option value="senior">Senior</option>
-        </select>
-      </li>
-      <li class="form-row">
-        <label for="language">Idioma</label>
-        <select id="language" v-model="newRate.language" required>
-          <option v-if="search" value="all" class="search-all">Todos</option>
-          <option value="spanish">Español</option>
-          <option value="english">Inglés</option>
-        </select>
-      </li>
-      <li class="form-row" v-if="!search">
-        <label for="salary">Salario promedio (centavos)</label>
-        <input
-          type="number"
-          id="salary"
-          min="0"
-          v-model="newRate.average_salary_in_cents"
-          required
-        />
-      </li>
-      <li class="form-row" v-if="!search">
-        <label for="gross-margin">Margen bruto (centavos)</label>
-        <input
-          type="number"
-          id="gross-margin"
-          min="0"
-          v-model="newRate.gross_margin_in_cents"
-          required
-        />
-      </li>
-      <li class="form-row">
-        <label for="currency">Moneda</label>
-        <select id="currency" v-model="newRate.currency" required>
-          <option value="ars">Peso argentino (ARS)</option>
-          <option value="usd">Dólar estadounidense (USD)</option>
-        </select>
-      </li>
-      <li class="form-row">
-        <button type="submit">Aceptar</button>
-        <button id="cancel" type="button" @click="cancel">Cancelar</button>
-      </li>
-    </ul>
+  <form class="mx-1" @submit.prevent="submit">
+    <div class="form-row">
+      <label class="form-label" for="technology">Tecnología</label>
+      <select
+        class="form-select"
+        id="technology"
+        v-model.number="newRate.technology_id"
+        required
+      >
+        <option v-if="search" value="all" class="search-all">Todas</option>
+        <option v-for="tech in technologies" :key="tech.id" :value="tech.id">
+          {{ tech.name }}
+        </option>
+      </select>
+    </div>
+    <div class="form-row">
+      <label class="form-label" for="seniority">Seniority</label>
+      <select
+        class="form-select"
+        id="seniority"
+        v-model="newRate.seniority"
+        required
+      >
+        <option v-if="search" value="all" class="search-all">Todas</option>
+        <option value="junior">Junior</option>
+        <option value="semi_senior">Semi senior</option>
+        <option value="senior">Senior</option>
+      </select>
+    </div>
+    <div class="form-row">
+      <label class="form-label" for="language">Idioma</label>
+      <select
+        class="form-select"
+        id="language"
+        v-model="newRate.language"
+        required
+      >
+        <option v-if="search" value="all" class="search-all">Todos</option>
+        <option value="spanish">Español</option>
+        <option value="english">Inglés</option>
+      </select>
+    </div>
+    <div class="form-row" v-if="!search">
+      <label class="form-label" for="salary">Salario promedio (centavos)</label>
+      <input
+        class="form-control"
+        type="number"
+        id="salary"
+        min="0"
+        v-model="newRate.average_salary_in_cents"
+        required
+      />
+    </div>
+    <div class="form-row" v-if="!search">
+      <label class="form-label" for="gross-margin">
+        Margen bruto (centavos)
+      </label>
+      <input
+        class="form-control"
+        type="number"
+        id="gross-margin"
+        min="0"
+        v-model="newRate.gross_margin_in_cents"
+        required
+      />
+    </div>
+    <div class="form-row">
+      <label class="form-label" for="currency">Moneda</label>
+      <select
+        class="form-select"
+        id="currency"
+        v-model="newRate.currency"
+        required
+      >
+        <option value="ars">Peso argentino (ARS)</option>
+        <option value="usd">Dólar estadounidense (USD)</option>
+      </select>
+    </div>
+    <div class="btn-group mt-3">
+      <button class="btn btn-primary" type="submit">Aceptar</button>
+      <button
+        class="btn btn-secondary"
+        id="cancel"
+        type="button"
+        @click="cancel"
+      >
+        Cancelar
+      </button>
+    </div>
   </form>
 </template>
 
@@ -123,55 +152,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-.wrapper {
-  background-color: whitesmoke;
-  list-style-type: none;
-  padding: 0;
-  border-radius: 3px;
-}
-.form-row {
-  display: flex;
-  justify-content: flex-end;
-  padding: 0.5em;
-}
-
-.form-row > button {
-  margin: 2px;
-}
-
-.form-row > label {
-  padding: 0.5em 1em 0.5em 0;
-  text-align: right;
-  flex: 1;
-}
-.form-row > input,
-.form-row > select {
-  flex: 2;
-}
-.form-row > input,
-.form-row > select,
-.form-row > button {
-  padding: 0.5em;
-}
-
-@media screen and (min-width: 768px) {
-  .form-row > input,
-  .form-row > select {
-    flex: 3;
-  }
-}
-@media screen and (min-width: 992px) {
-  .form-row > input,
-  .form-row > select {
-    flex: 4;
-  }
-}
-@media screen and (min-width: 1200px) {
-  .form-row > input,
-  .form-row > select {
-    flex: 5;
-  }
-}
-</style>
