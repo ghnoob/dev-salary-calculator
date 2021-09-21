97ba2f10f7c53ced6a847edf661992ec5bd26eaf
ghnoob <ptnchk.rodrigo@gmail.com>
2021-09-21 12:27:23 -0300

    style home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,40 +1,43 @@
 <template>
-  <h2>Página principal</h2>
-  <p>¿Qué desea hacer?</p>
-  <nav>
-    <router-link class="link" :to="{ name: 'Technologies' }">
-      Consultar las tecnologías disponibles
-    </router-link>
-    <router-link class="link" :to="{ name: 'RateList' }">
-      Consultar, modificar y eliminar las tarifas
-    </router-link>
-    <router-link class="link" :to="{ name: 'NewRate' }">
-      Agregar una nueva tarifa
-    </router-link>
-    <router-link class="link" :to="{ name: 'CalcRatesParams' }">
-      Calcular tarifas
-    </router-link>
-  </nav>
+  <div class="home">
+    <h2>Página principal</h2>
+    <div class="container d-flex flex-column justify-center h-100">
+      <h3>Acerca del proyecto</h3>
+      <p>
+        Esta una herramienta pensada para que los moderadores de comunidades de
+        Devs puedan ayudar a sus miembros a entender, según una serie de
+        criterios, cuál es el salario promedio que maneja la industria para cada
+        caso. Los profesionales que reciben la orientación, tienen la
+        posibilidad de retribuir un 1% de su primer salario para que ese
+        ecosistema que lo acompañó pueda seguir creciendo.
+      </p>
+      <h3>Partes del sitio</h3>
+      <ul class="list-unstyled">
+        <li><span class="lead">Home:</span> esta página</li>
+        <li>
+          <span class="lead">Tecnologías:</span> consultar las tecnologías
+          disponibles para trabajar
+        </li>
+        <li>
+          <span class="lead">Tarifas</span>
+          <ul>
+            <li>
+              <span class="lead">Ver:</span> una tabla con las tarifas creadas
+              hasta el momento. Se pueden crear tarifas nuevas, o editar o
+              eliminar una existente.
+            </li>
+            <li>
+              <span class="lead">Crear:</span> un formulario que permite crear
+              una nueva tarifa y agregarla a la base de datos
+            </li>
+            <li>
+              <span class="lead">Calcular:</span> se introducen criterios de
+              búsqueda y se calculan los salarios promedio para ese tipo de
+              desarrolladores
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
-
-<style scoped>
-nav {
-  display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
-}
-
-nav .link {
-  margin: 0.3em;
-}
-
-.home {
-  display: flex;
-  height: 100%;
-  flex-direction: column;
-}
-
-.home .content {
-  flex: 1;
-}
-</style>
